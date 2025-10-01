1746e746871998def1a3b70d9a422e71dbf44e76
cosbik-Gysse8-reqjuv <lol25playz@gmail.com>
2025-10-01 23:03:40 +0000

    commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the matrix
-A = np.array([[1.8, -0.9],
+A = np.array([[1.9002, -0.9],
               [1.0,  0.0]])
 
 # Compute A^x for x from 1 to 200 and collect top-left element (position [0, 0])
@@ -14,9 +14,9 @@ top_left_values = [np.linalg.matrix_power(A, x)[0, 0] for x in powers]
 # Plot the results
 plt.figure(figsize=(12, 6))
 plt.plot(powers, top_left_values, label='Top-left entry of A^x')
-plt.xlabel('x (Power)')
-plt.ylabel('Top-left value of A^x')
-plt.title('Top-left Corner of Matrix [[1.8, -0.9], [1, 0]]^x for x from 1 to 200')
+plt.xlabel('k (years after shock)')
+plt.ylabel('Impulse response')
+plt.title('Impulse Response Over Time')
 plt.grid(True)
 plt.legend()
 plt.tight_layout()
